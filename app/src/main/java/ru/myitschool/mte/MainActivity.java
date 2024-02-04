36f1cef63f237fe1708f916552be75c7cd1ab344
Serafim <serafimpankin@yandex.ru>
2024-02-05 00:45:43 +0300

    ДЗ ГОТОВО!

--- app/src/main/java/ru/myitschool/mte/MainActivity.java
+++ app/src/main/java/ru/myitschool/mte/MainActivity.java
@@ -3,7 +3,6 @@ package ru.myitschool.mte;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import ru.myitschool.mte.databinding.ActivityMainBinding;
 
